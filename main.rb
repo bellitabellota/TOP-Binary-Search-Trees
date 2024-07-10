3d13e77a38482e0a5ddbd58d41714d80957613df
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-07-10 17:19:40 +0100

    Create #delete function

--- main.rb
+++ main.rb
@@ -2,14 +2,22 @@ require_relative "lib/node"
 require_relative "lib/tree"
 
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-# tree.pretty_print
+ tree.pretty_print
 
 # p tree.root.data
 # p tree.root.left_child.data
 
-#tree.insert(24)
+# tree.insert(70)
 tree.pretty_print
 
 # deleting_leaf_node(3)
 
-p tree.find(6)
+# tree.find(6)
+
+p tree.root
+
+tree.delete(4)
+
+tree.pretty_print
+#p tree.root
+
