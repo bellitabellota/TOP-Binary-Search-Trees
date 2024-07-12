4cd325419a1b25f4e0f00b22bb06cc10e03bcd53
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-07-12 16:09:18 +0100

    Create driver script according tothe TOP instructions

--- main.rb
+++ main.rb
@@ -1,39 +1,32 @@
 require_relative "lib/node"
 require_relative "lib/tree"
 
-tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
- tree.pretty_print
-
-# p tree.root.data
-# p tree.root.left_child.data
-
-# tree.insert(70)
-# tree.pretty_print
-
-# deleting_leaf_node(3)
-
-# tree.level_order_iterative
-# tree.level_order_recursive { |node| p node.data }
-
-#tree.preorder { |node| puts node.data }
-
-# tree.inorder
-# tree.postorder { |node| puts node.data }
-
-# p tree.height(4)
-
-# p tree.depth(23)
-
-#p tree.balanced?
-
-tree.delete(5)
-tree.delete(7)
-tree.delete(3)
-tree.delete(1)
-
+tree = Tree.new(Array.new(15) { rand(1..100) })
 tree.pretty_print
 
 p tree.balanced?
 
+p tree.level_order_iterative
+p tree.level_order_recursive
+
+p tree.preorder
+p tree.inorder
+p tree.postorder
+
+5.times do
+  tree.insert(rand(100..200))
+end
+
+tree.pretty_print
+p tree.balanced?
+
 tree.rebalance
 tree.pretty_print
+p tree.balanced?
+
+p tree.level_order_iterative
+p tree.level_order_recursive
+
+p tree.preorder
+p tree.inorder
+p tree.postorder
